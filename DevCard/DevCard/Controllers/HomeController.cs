00a5de653b9b4527465c59fe69191700ff0b4e79
paamir <apr1385apsm@gmail.com>
2022-02-03 12:14:08 +0330

    second

--- DevCard/DevCard/Controllers/HomeController.cs
+++ DevCard/DevCard/Controllers/HomeController.cs
@@ -11,12 +11,8 @@ namespace DevCard.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+
 
         public IActionResult Index()
         {
