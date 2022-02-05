d1994764cd27c6b7ec6fc1d6e7fe63ad200151ad
paamir <apr1385apsm@gmail.com>
2022-02-05 20:23:48 +0330

    index & contact added

--- DevCard/DevCard/Controllers/HomeController.cs
+++ DevCard/DevCard/Controllers/HomeController.cs
@@ -1,21 +1,15 @@
 ﻿using DevCard.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCard.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -23,7 +17,7 @@ namespace DevCard.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
